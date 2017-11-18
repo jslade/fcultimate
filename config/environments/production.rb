96e9b3a3522e617f87ea1a6fe6f2b6c6f1f183e6
Jeremy Slade <jeremy@jkslade.net>
2017-11-17 21:48:09 -0700

    Disable assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.secret_key = ENV['SECRET_KEY_BASE']
-  config.build_production_command = 'echo skipping assets:precompile'
 end
