b71fa2b0d18cf16d4baf5b79f748d0fdaf5a1cfa
Jeremy Slade <jeremy@jkslade.net>
2017-11-20 22:17:25 -0700

    Routing fix to work with react-router

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   protect_from_forgery with: :exception
+
+  # For any urls that don't match explicitly-configured routes, fallback
+  # to serving the root index.html, which will mean that url is handled
+  # by react-router on the frontend
+  def fallback
+    render file: 'public/index.html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
 
     get 'contents/:name', to: 'contents#show', constraints: { name: %r{[^/?]+} }
   end
+
+  # This is needed for compatibility with react-router on the frontend, so none
+  # of its routes get processed by rails, but rather everything gets processed
+  # through the public/index_html
+  get '*path', to: 'application#fallback', constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
 end
