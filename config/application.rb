c8914b7b40fed5206add6b8b3f3f73ee7ac00b91
Jeremy Slade <jeremy@jkslade.net>
2017-11-20 21:37:44 -0700

    Config fix for /admin

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Api
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.api_only = true
-    config.middleware.use ActionDispatch::Flash
+    #config.api_only = true
+    #config.middleware.use ActionDispatch::Flash
   end
 end
